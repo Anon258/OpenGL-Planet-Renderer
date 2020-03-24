a30e75d7b019ee34bcbd7ef6dc413202c0b5b82e
Raj Aryan Agrawal <noname14274@gmail.com>
2020-03-25 03:36:00 +0530

    Added the glm library to utilities

---
+++ Utility/Cube.hpp
@@ -0,0 +1,18 @@
+#ifndef _COLORCUBE_HPP_
+#define _COLORCUBE_HPP_
+
+#include<iostream>
+#include<cmath>
+
+//GLEW
+#define GLEW_STATIC
+#include<GL/glew.h>
+
+//GLFW
+#include<GLFW/glfw3.h>
+
+#include "Utility/Shader.hpp"
+#include "Utility/glm/vec4.hpp"
+#include "Utility/glm/mat4x4.hpp"
+
+#endif
